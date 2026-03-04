4f267e8b173c2e909af1026dbd7df7296ceca6c6
Albeeertt <alberto.sanchezsoto16@gmail.com>
2026-03-04 08:51:33 +0100

    remove confusion matrix

--- kmasgec/core/models/epochs/epoch.py
+++ kmasgec/core/models/epochs/epoch.py
@@ -304,12 +304,12 @@ def iteration_test_oneHead(
     print(f"Accuracy en test: {epoch_acc:.4f}")
     print("--------------------------------------------------")
     report_dict = classification_report(all_trues, all_preds, digits=4, output_dict=True)
-    cm = confusion_matrix(
-        all_trues,
-        all_preds
-    )
-    cm_list = cm.tolist()
-    report_dict["confusion_matrix"] = cm_list
+    # cm = confusion_matrix(
+    #     all_trues,
+    #     all_preds
+    # )
+    # cm_list = cm.tolist()
+    # report_dict["confusion_matrix"] = cm_list
 
     print("Fin")
     return report_dict, all_trues, all_preds, all_places, all_softmax_official_values
